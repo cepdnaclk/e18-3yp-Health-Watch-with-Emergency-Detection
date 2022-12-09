a877642abbf35998d14527c37302cbc738a35f7e
Jayathri Ranasinghe <e18283@eng.pdn.ac.lk>
2022-12-09 21:42:00 +0530

    new App.js file added

--- MediCare/App.js
+++ MediCare/App.js
@@ -1,100 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button, ImageBackground, TouchableOpacity } from 'react-native';
+import HomeScreen from "./screens/HomeScreen";
+import SecondScreen from "./screens/SecondScreen";
+import ThirdScreen from "./screens/ThirdScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function sample() { }
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ImageBackground source={require('./assets/bglogo2.jpg')} resizeMode={'cover'} style={styles.backgroundImage}>
-      <View style={styles.appContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.textInput} placeholder='Username' />
-          <TextInput style={styles.textInput} placeholder='Password' />
-          <Text style={styles.textStyle}>Forgot password?</Text>
-          <View style={styles.buttonStyle}>
-            <TouchableOpacity onPress={sample} style={styles.button}>
-              <Text style={styles.buttonText}>LOG IN</Text>
-            </TouchableOpacity>
-          </View>
-          <Text style={{alignSelf: 'center', color: '#ADADAD', paddingTop:8}}>Don't have an account?</Text>
-          <View style={styles.buttonStyle}>
-            <TouchableOpacity onPress={sample} style={styles.button}>
-              <Text style={styles.buttonText}>SIGN UP</Text>
-            </TouchableOpacity>
-          </View>
-          {/* <View style={styles.buttonStyle}>
-            <Button style={styles.button} title="Log in" />
-          </View> */}
-          {/* <View style={styles.buttonStyle}>
-            <Button style={styles.button} title="Sign up" />
-          </View > */}
-
-        </View>
-
-      </View>
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={{title: "Welcome"}}
+        />
+        <Stack.Screen 
+          name="Login"
+          component={LoginScreen}
+          options={{title: "Log in"}}
+        />
+        <Stack.Screen 
+          name="Second"
+          component={SecondScreen}
+        />
+        <Stack.Screen 
+          name="Third"
+          component={ThirdScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 250,
-    padding: 50,
-    alignContent: 'center',
-    justifyContent: 'center',
-
-  },
-
-  textStyle: {
-    color: '#ADADAD',
-    alignSelf: 'flex-end',
-  },
-
-  inputContainer: {
-    paddingTop: 50,
-    padding: 5,
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    wdith: 80,
-    height: 40,
-    padding: 10,
-    marginBottom: 17,
-  },
-
-  buttonStyle: {
-    marginHorizontal: '6%',
-    marginTop: 10,
-    marginBottom: 50,
-    width: '90%',
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-
-  button: {
-    marginHorizontal: '6%',
-    width: '80%',
-    height: 40,
-    backgroundColor: "#4693FC",
-    padding: 10,
-    borderRadius: 30
-  },
-
-  buttonText: {
-    color: "white",
-    alignSelf: 'center',
-  }
-
-});
