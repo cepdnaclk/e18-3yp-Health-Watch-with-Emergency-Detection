d6c0d315d6cae4b6e4d2b84d35f99697957aa3f2
Sanjaya Gihan <e18412@eng.pdn.ac.lk>
2022-12-22 08:31:56 +0530

    Update NetworkHandler.dart

--- code/frontend/lib/NetworkHandler.dart
+++ code/frontend/lib/NetworkHandler.dart
@@ -4,8 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NetworkHandler {
-  
-  String baseurl = "https://3yp-new-app.azurewebsites.net/";
+  String baseurl = "https://medicare3ypnew.azurewebsites.net/";
 
   Future<String> get(String url, String token) async {
     url = formater(url);
@@ -21,34 +20,58 @@ class NetworkHandler {
     return "";
   }
 
-
-  Future<int> updateReminders(String url, Map<String, Map<String, String>> body) async {
+  Future<int> updateReminders(
+      String url, Map<String, Map<String, String>> body) async {
     url = formater(url);
-    
-    var response = await http.post(Uri.parse(url),headers: {"Content-type":"application/json"} ,body:json.encode(body));
-    
-    if(response.statusCode == 200 || response.statusCode ==201){
+
+    var response = await http.post(Uri.parse(url),
+        headers: {"Content-type": "application/json"}, body: json.encode(body));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
       // ignore: avoid_print
       print("testing:$body");
       // ignore: avoid_print
       print(json.decode(response.body));
       return response.statusCode;
-    } return response.statusCode;
-    
+    }
+    return response.statusCode;
   }
 
+  Future<String> getContacts(String url) async {
+    url = formater(url);
+
+    var response = await http
+        .get(Uri.parse(url), headers: {"Content-type": "application/json"});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.body;
+    }
+    return "";
+  }
 
   Future<String> getReminders(String url) async {
-
     url = formater(url);
-    
-    var response = await http.get(Uri.parse(url),headers: {"Content-type":"application/json"});
-    
-   if(response.statusCode == 200 || response.statusCode ==201){
+
+    var response = await http
+        .get(Uri.parse(url), headers: {"Content-type": "application/json"});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
       return response.body;
-    }return "";
+    }
+    return "";
   }
 
+  Future<int> deleteReminders(String url, Map<String, String> body) async {
+    url = formater(url);
+
+    var response = await http.patch(Uri.parse(url),
+        headers: {"Content-type": "application/json"}, body: json.encode(body));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.statusCode;
+    }
+    return response.statusCode;
+  }
 
   Future<String> post(String url, Map<String, String> body) async {
     url = formater(url);
@@ -70,4 +93,3 @@ class NetworkHandler {
     return baseurl + url;
   }
 }
-
